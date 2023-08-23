6ffb2b4af9cd25eda622cb9ed1d58489c9ce49cc
MagnusVV <112693199+MagnusVV@users.noreply.github.com>
2023-08-23 14:45:54 +0200

    Animations almost done!
    Used framer motion to animate a "door" effect.

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,40 +1,62 @@
 import styled from "styled-components";
 import React from "react";
+import { useRef } from "react";
 import { motion, useScroll, useTransform } from "framer-motion";
 
 const Hero = () => {
-  const { scrollYProgress } = useScroll();
+  const targetRef = useRef(null);
 
-  const x = useTransform(scrollYProgress, [0, 1], ["50%", "-95%"]);
+  const { scrollYProgress } = useScroll({ target: targetRef });
 
   return (
     <>
-      <HeroContainer>
-        <motion.div style={{ x }}>
-          <LeftSplitHero>SAMMAN</LeftSplitHero>
-        </motion.div>
-        <motion.div style={{ x }}>
-          <SplitHero>SLAGET</SplitHero>
-        </motion.div>
-      </HeroContainer>
-      <div>TEST</div>
+      <HeroAnimationWrapper ref={targetRef}>
+        <HeroContainer>
+          <motion.div
+            style={{
+              x: useTransform(scrollYProgress, [0, 1], ["0%", "-125%"]),
+            }}
+          >
+            <LeftSplitHero>SAMMAN</LeftSplitHero>
+          </motion.div>
+          <motion.div
+            style={{ x: useTransform(scrollYProgress, [0, 1], ["0%", "125%"]) }}
+          >
+            <RightSplitHero>SLAGET</RightSplitHero>
+          </motion.div>
+        </HeroContainer>
+        <HeroArticle>TEST!</HeroArticle>
+      </HeroAnimationWrapper>
     </>
   );
 };
 
 const HeroContainer = styled.div`
-  background-color: yellow;
   width: 100%;
   height: 100vh;
   display: flex;
   align-items: center;
   z-index: 2;
   position: sticky;
-  /* left: 0;
-  top: 0; */
+  left: 0;
+  top: 0;
+  div {
+    display: flex;
+    width: 50vw;
+    height: 100%;
+  }
 `;
 
-const SplitHero = styled.div`
+const HeroArticle = styled.article`
+  width: 100%;
+  position: sticky;
+  left: 0;
+  top: 0;
+  text-align: center;
+`;
+
+// These two styled divs split to left and right during initial scrolling.
+const RightSplitHero = styled.div`
   font-size: 10rem;
   background-color: black;
   height: 100%;
@@ -43,8 +65,13 @@ const SplitHero = styled.div`
   align-items: center;
 `;
 
-const LeftSplitHero = styled(SplitHero)`
+const LeftSplitHero = styled(RightSplitHero)`
   justify-content: end;
+`;
+
+// Prevents scrolling down until the sliders are completely off screen.
+const HeroAnimationWrapper = styled.section`
+  height: 200vh;
 `;
 
 export default Hero;
