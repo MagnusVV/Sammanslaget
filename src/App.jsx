0df1d0f001a075a6c8cec898d3ed8b23a45041ff
Styrbjörn Nordberg <styris.n@gmail.com>
2023-08-23 09:30:45 +0200

    created basic component exports and imported them

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,8 @@
 import { useState } from "react";
+import Hero from "./components/hero/hero";
+import Game from "./components/game/game";
+import Slide from "./components/slide/slide";
+import About from "./components/about/about";
 import "./App.css";
 
 function App() {
