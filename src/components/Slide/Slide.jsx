2a9800749298ce8d086714b33163a6f47a2841b8
Styrbjörn Nordberg <styris.n@gmail.com>
2023-08-24 10:05:26 +0200

    slide styling

--- src/components/Slide/Slide.jsx
+++ src/components/Slide/Slide.jsx
@@ -1,14 +1,30 @@
-import styled from "styled-components";
-import React from "react";
+import styled from 'styled-components';
+import React from 'react';
 
 const Slide = (props) => {
   return (
-    <div className="Slide">
-      <h2>{props.title}</h2>
-      <p>{props.text}</p>
+    <BadNameChangeLater>
       <img src={props.img} alt="" />
-    </div>
+      <div>
+        <h2>{props.title}</h2>
+        <p>{props.text}</p>
+      </div>
+    </BadNameChangeLater>
   );
 };
+
+const BadNameChangeLater = styled.div`
+  height: 35vh;
+  display: flex;
+  justify-content: space-between;
+  margin-bottom: 10rem;
+  padding-inline: 10rem;
+  div {
+    width: 50%;
+    display: flex;
+    flex-direction: column;
+    gap: 3rem;
+  }
+`;
 
 export default Slide;
