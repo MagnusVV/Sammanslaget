790e061896bff3f0a0f0e42fc76895dc72a7fb7c
Styrbjörn Nordberg <styris.n@gmail.com>
2023-08-25 15:06:34 +0200

    disabled unity in moblie and tablet

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,49 @@
-import { useState } from "react";
-import Hero from "./components/Hero/Hero";
-import Game from "./components/Game/Game";
-import Slide from "./components/Slide/Slide";
-import About from "./components/About/About";
-import "./App.css";
-import { styled } from "styled-components";
-import { devices } from "./Breakpoints";
+import { useState } from 'react';
+import Hero from './components/Hero/Hero';
+import Game from './components/Game/Game';
+import Slide from './components/Slide/Slide';
+import About from './components/About/About';
+import './App.css';
+import { styled } from 'styled-components';
+import { devices } from './Breakpoints';
 
-import SelectionTestGif from "../public/Imgs/selectionTest.gif";
+import SelectionTestGif from '../public/Imgs/selectionTest.gif';
 
-import { Unity, useUnityContext } from "react-unity-webgl";
+import { Unity, useUnityContext } from 'react-unity-webgl';
 
 function App() {
+  var isDesktop = false;
+  if (window.innerWidth >= 1024) {
+    var isDesktop = true;
+  }
+  console.log(isDesktop);
+
   const textLorem =
-    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere, nisi molestias. Velit magni optio earum at maxime non necessitatibus reiciendis provident repudiandae corporis? Eius repudiandae rerum odit eligendi at. Quisquam?";
+    'Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere, nisi molestias. Velit magni optio earum at maxime non necessitatibus reiciendis provident repudiandae corporis? Eius repudiandae rerum odit eligendi at. Quisquam?';
   const groupNames = [
-    "Kevin - GP",
-    "Joel - UX",
-    "Magnus - WU",
-    "Styrbjörn - WU",
+    'Kevin - GP',
+    'Joel - UX',
+    'Magnus - WU',
+    'Styrbjörn - WU',
   ];
   const groupImgs = [
-    "/Imgs/Kevin.png",
-    "/Imgs/Joel.jpg",
-    "/Imgs/Magnus.jpg",
-    "/Imgs/Styrbjorn.jpg",
+    '/Imgs/Kevin.png',
+    '/Imgs/Joel.jpg',
+    '/Imgs/Magnus.jpg',
+    '/Imgs/Styrbjorn.jpg',
   ];
   const groupIntro = [
-    "GP-student som gillar att spela och skapa spel.",
-    "26 år gammal UX-design student, som tycker om att spela musik och laga mat på fritiden. Singel.  ",
-    "WU-student i sina bästa år. Lyssnar också mycket på musik och tycker om dystopisk film. ",
-    "22 årig WU student. Lyssnar mycket på musik på fritiden.",
+    'GP-student som gillar att spela och skapa spel.',
+    '26 år gammal UX-design student, som tycker om att spela musik och laga mat på fritiden. Singel.  ',
+    'WU-student i sina bästa år. Lyssnar också mycket på musik och tycker om dystopisk film. ',
+    '22 årig WU student. Lyssnar mycket på musik på fritiden.',
   ];
 
   const { unityProvider } = useUnityContext({
-    loaderUrl: "/Build/Build.loader.js",
-    dataUrl: "/Build/Build.data",
-    frameworkUrl: "/Build/Build.framework.js",
-    codeUrl: "/Build/Build.wasm",
+    loaderUrl: '/Build/Build.loader.js',
+    dataUrl: '/Build/Build.data',
+    frameworkUrl: '/Build/Build.framework.js',
+    codeUrl: '/Build/Build.wasm',
   });
 
   return (
@@ -52,39 +58,39 @@ function App() {
           urvalsprov.
         </SlideDescription>
         <Slide
-          title={"Kompetens, logik och resonemang:"}
+          title={'Kompetens, logik och resonemang:'}
           text={
-            "Första delen av urvalsprovet görs individuellt där personen får utföra allmänna kognitiva uppgifter, svara på flervalsfrågor kring ämnet samt fritextfrågor där personen behöver resonera kring en lösning till ett specifikt problem. Denna del sker på plats och frågorna besvaras på en dator, det skall finnas en testledare på plats för att hjälpa till om något strular samt ha ett överseende så att inget fusk äger rum. "
+            'Första delen av urvalsprovet görs individuellt där personen får utföra allmänna kognitiva uppgifter, svara på flervalsfrågor kring ämnet samt fritextfrågor där personen behöver resonera kring en lösning till ett specifikt problem. Denna del sker på plats och frågorna besvaras på en dator, det skall finnas en testledare på plats för att hjälpa till om något strular samt ha ett överseende så att inget fusk äger rum. '
           }
-          img={"https://source.unsplash.com/oXV3bzR7jxI"}
+          img={'https://source.unsplash.com/oXV3bzR7jxI'}
           alt={
-            "Person vid ett bord fyller i ett papper med en penna. Bara personens händer syns."
+            'Person vid ett bord fyller i ett papper med en penna. Bara personens händer syns.'
           }
         />
         <Slide
-          title={"Kreativ problemlösning:"}
+          title={'Kreativ problemlösning:'}
           text={
-            "Nästa moment är att återskapa en 3D-figur med hjälp av olika byggblock genom att placera dem utefter en ritning. Problemet är att byggblocken inte stämmer med dem i ritningen och kräver att personen behöver tänka kreativt för att återskapa figuren. På detta sätt kan vi se hur testpersonen kan lösa ett tidspressat problem med hjälp av sin kreativa förmåga. Till vänster ser ni en demo på vår prototyp.  "
+            'Nästa moment är att återskapa en 3D-figur med hjälp av olika byggblock genom att placera dem utefter en ritning. Problemet är att byggblocken inte stämmer med dem i ritningen och kräver att personen behöver tänka kreativt för att återskapa figuren. På detta sätt kan vi se hur testpersonen kan lösa ett tidspressat problem med hjälp av sin kreativa förmåga. Till vänster ser ni en demo på vår prototyp.  '
           }
           img={SelectionTestGif}
           alt={
-            "GIF som visar ett utsnitt av hur prototypen fungerar, med block som flyttas omkring och fästs på en bottenplatta."
+            'GIF som visar ett utsnitt av hur prototypen fungerar, med block som flyttas omkring och fästs på en bottenplatta.'
           }
         />
 
         {/* <Unity unityProvider={unityProvider} /> */}
 
         <Slide
-          title={"Kommunikation och samarbete"}
+          title={'Kommunikation och samarbete'}
           text={
-            "Den sista delen av urvalsprovet är en gruppuppgift där flera sökande får jobba i grupp där under viss tid lösa ett problem tillsammans. Syftet med denna uppgift är att avgöra hur väl deltagarna kommunicerar sina resonemang samt hur de samarbetar med de andra för att lösa problemet. En testledare är närvarande för att förklara uppgiften och för anteckningar på hur varje deltagare tar sig an uppgiften. "
+            'Den sista delen av urvalsprovet är en gruppuppgift där flera sökande får jobba i grupp där under viss tid lösa ett problem tillsammans. Syftet med denna uppgift är att avgöra hur väl deltagarna kommunicerar sina resonemang samt hur de samarbetar med de andra för att lösa problemet. En testledare är närvarande för att förklara uppgiften och för anteckningar på hur varje deltagare tar sig an uppgiften. '
           }
-          img={"https://source.unsplash.com/-uHVRvDr7pg"}
+          img={'https://source.unsplash.com/-uHVRvDr7pg'}
           alt={
-            "Fyra personer sitter och diskuterar någonting. Två av dem ler vänligt. En tredje ser allvarlig ut och tar anteckningar, medan bara bakhuvudet på den fjärde syns."
+            'Fyra personer sitter och diskuterar någonting. Två av dem ler vänligt. En tredje ser allvarlig ut och tar anteckningar, medan bara bakhuvudet på den fjärde syns.'
           }
         />
-        <Unity unityProvider={unityProvider} />
+        {isDesktop == true && <Unity unityProvider={unityProvider} />}
       </section>
       <About text={groupIntro} img={groupImgs} name={groupNames} />
     </>
